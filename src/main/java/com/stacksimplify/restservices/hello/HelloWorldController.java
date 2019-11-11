122c14f15a5b215056c82e57c945569864bf2142
Jayamaruthyraman Jaganathan <jayjag04@gmail.com>
2019-11-10 23:05:25 -0500

    i18n - first commit

--- src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -1,20 +1,35 @@
 package com.stacksimplify.restservices.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 // controller
 @RestController
 public class HelloWorldController {
-	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+
 	//@RequestMapping(method=RequestMethod.GET, path= "/helloworld")
 	@GetMapping("/helloworld1")
 	public String helloWorld() {
 		return "Hello World!1";
 	}
+
+	@GetMapping("hello-i18n")
+	public String getMessagesInI18NFormat(@RequestHeader(name="Accept-Language", required=false) String locale)   {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
 	
+	@GetMapping("hello-i18n2")
+	public String getMessagesInI18NFormat2()   {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+	}
 	
 	@GetMapping("/helloworld-bean")
 	public UserDetails helloWorldBean() {
